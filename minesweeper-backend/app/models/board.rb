36b5cb8eb34b3cb8a2ef9187279f1b6989870068
Newton Garcia <newxhost@gmail.com>
2021-10-10 15:44:10 -0300

    Board#to_table to print the board to console

--- minesweeper-backend/app/models/board.rb
+++ minesweeper-backend/app/models/board.rb
@@ -9,25 +9,31 @@ class Board
     @height = height
   end
 
-  # Public: prints the board to console.
-  def show
-    grid.each do |row|
-      row.each do |cell|
-        puts cell.to_s
+  # Public: prints the board as a table of cells to console.
+  def to_table
+    Terminal::Table.new do |t|
+      grid.each_with_index do |row, row_index|
+        t.add_row row.map(&:to_s)
+        t.add_separator unless last_grid_row?(row_index)
       end
     end
   end
-
-  private
 
   # Private: initializes the grid.
   def grid
     @grid ||= create_grid
   end
 
+  private
+
   # Creates a grid of cells.
   def create_grid
     # TODO: maybe we should initialize the cells already with randomly mines here.
     Array.new(width) { Array.new(height) { Cell.new } }
+  end
+
+  # Private: Returns true if the given row index is the last row of the grid.
+  def last_grid_row?(row_index)
+    row_index == grid.length - 1
   end
 end
--- minesweeper-backend/spec/models/board_spec.rb
+++ minesweeper-backend/spec/models/board_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Board.new }
+
+  describe "#grid" do
+    it "returns a grid with default size" do
+      expect(subject.grid.length).to eq(Board::DEFAULT_WIDTH)
+      expect(subject.grid[0].length).to eq(Board::DEFAULT_HEIGHT)
+    end
+  end
+
+  describe "#to_table" do
+    it "each printed grid cell matches a grid cell position" do
+      table = subject.to_table
+
+      puts table
+
+      subject.grid.transpose.each_index do |column_index|
+        expect(table.column(column_index)).to eq(subject.grid.transpose[column_index].map(&:to_s))
+      end
+    end
+  end
 end
